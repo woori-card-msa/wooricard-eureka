ab34e15f4cf487667dd0dc2c7144ead2d9d315d6
Minyeong <skaalsdud7@gmail.com>
2026-03-20 10:37:48 +0900

    feat: Eureka 서버 초기 구축 및 기본 설정

--- src/main/java/com/wooricard/wooricard_eureka/WooricardEurekaApplication.java
+++ src/main/java/com/wooricard/wooricard_eureka/WooricardEurekaApplication.java
@@ -2,8 +2,10 @@ package com.wooricard.wooricard_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // 유레카 서버 역할 활성화
 public class WooricardEurekaApplication {
 
 	public static void main(String[] args) {
