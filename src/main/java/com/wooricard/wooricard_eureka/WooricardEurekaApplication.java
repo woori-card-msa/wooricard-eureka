79aa9884b023e051db8d4fb8240641827b8b950f
Minyeong <skaalsdud7@gmail.com>
2026-03-20 10:19:34 +0900

    Initial commit

---
+++ src/main/java/com/wooricard/wooricard_eureka/WooricardEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.wooricard.wooricard_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooricardEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooricardEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wooricard/wooricard_eureka/WooricardEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wooricard.wooricard_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooricardEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
